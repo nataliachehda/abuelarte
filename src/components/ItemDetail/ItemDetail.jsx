33036d85d2dcba7cce345f515912f1ed52b52c9f
nataliachehda <nataliachehda@gmail.com>
2023-07-23 00:01:57 -0300

    css

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,36 +1,46 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import ItemCount from '../ItemCount/ItemCount'
-import './ItemDetail.css'
-import { CarritoContext } from '../../context/CarritoContext'
-import { useContext } from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import ItemCount from '../ItemCount/ItemCount';
+import './ItemDetail.css';
+import { CarritoContext } from '../../context/CarritoContext';
+import { useContext } from 'react';
 
+const ItemDetail = ({ id, nombre, precio, img, descripcion, stock }) => {
+  const [agregarCantidad, setAgregarCantidad] = useState(0);
+  const { agregarProducto } = useContext(CarritoContext);
 
-const ItemDetail = ({ id, nombre, precio, img,descripcion, stock }) => {
-    const [agregarCantidad, setAgregarCantidad] = useState(0);
+  const manejadorCantidad = (cantidad) => {
+    setAgregarCantidad(cantidad);
+    const item = { id, nombre, precio };
+    agregarProducto(item, cantidad);
+  };
 
-    const {agregarProducto} = useContext (CarritoContext);
+  return (
+    <div className='contenedorItem tarjeta'>
+      <h2> {nombre} </h2>
+      <h3>Precio: ${precio} </h3>
+      <h5>ID: {id} </h5>
+      <p> Descripción: {descripcion}</p>
+      <img src={img} alt={nombre} />
+      {agregarCantidad > 0 ? (
+        <>
+          <Link to="/cart" className="btn btn-primary">
+            Terminar compra
+          </Link>
+          <Link to="/" className="btn btn-secondary">
+            Ver más productos
+          </Link>
+        </>
+      ) : (
+        <>
+          <ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />
+          <Link to="/" className="btn btn-secondary">
+            Ver más productos
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
 
-    const manejadorCantidad = (cantidad) => {
-        setAgregarCantidad(cantidad);
-       // console.log("Productos agregados: " + cantidad);
-
-    const item = {id, nombre, precio};
-    agregarProducto (item, cantidad);
-    }
-
-    return (
-        <div className='contenedorItem tarjeta'>
-            <h2> {nombre} </h2>
-            <h3>Precio: ${precio} </h3>
-            <h5>ID: {id} </h5>
-            <p> Descripción: {descripcion}</p>
-            <img src={img} alt={nombre} />
-        {
-        agregarCantidad > 0 ? (<Link to="/cart" className="btn btn-primary"> Terminar compra </Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/>)
-        }
-        </div>
-    )
-}
-
-export default ItemDetail
+export default ItemDetail;
